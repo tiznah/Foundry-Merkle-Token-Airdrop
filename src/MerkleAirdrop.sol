a51400216a2c865392950c1a4ddc870b90426ba5
Your Name <you@example.com>
2025-05-15 00:58:47 -0700

    deploy scripts and tests

--- src/MerkleAirdrop.sol
+++ src/MerkleAirdrop.sol
@@ -3,33 +3,56 @@ pragma solidity ^0.8.24;
 
 import {IERC20, SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
-
-contract MerkleAirdrop {
+import {EIP712} from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+contract MerkleAirdrop is EIP712 {
     // some list of addresses and allow them to claim tokens
     address[] public claimers;
     bytes32 public i_merkleRoot;
     IERC20 private immutable i_bagleToken;
     mapping(address claimer => bool isClaimed) private s_claimed;
 
+    // EIP712
+    bytes32 private constant MESSAGE_TYPEHASH = keccak256("AirdropClaim(address account,uint256 amount)");
+    struct AirdropClaim {
+        address account;
+        uint256 amount;
+    }   
+
     using SafeERC20 for IERC20; // to avoid reverts from ERC20
 
     error MerkleAirdrop__InvalidProof();
     error MerkleAirdrop__AlreadyClaimed();
-
+    error MerkleAirdrop__InvalidSignature();
     event Claimed(address indexed account, uint256 amount);
 
-    constructor(address _bagleToken, bytes32 _merkleRoot) {
+    constructor(address _bagleToken, bytes32 _merkleRoot) EIP712("MerkleAirdrop", "1") {
         i_bagleToken = IERC20(_bagleToken);
         i_merkleRoot = _merkleRoot;
     }
 
-    function claim(address _account, uint256 _amount, bytes32[] calldata _merkleProof) external {
+    function claim(address _account, uint256 _amount, bytes32[] calldata _merkleProof, uint8 v, bytes32 r, bytes32 s) external {
         require(!s_claimed[_account], MerkleAirdrop__AlreadyClaimed());
+        // checking if the signature it valid if not revert
+        // message is the digest
+        if (!_isValidSignature(_account, getMessage(_account, _amount), v, r, s)) {
+            revert MerkleAirdrop__InvalidSignature();
+        }
         bytes32 leaf = keccak256(bytes.concat(keccak256(abi.encode(_account, _amount)))); // avoid second preeimage attack by hashing twice
         require(MerkleProof.verify(_merkleProof, i_merkleRoot, leaf), MerkleAirdrop__InvalidProof());
         emit Claimed(_account, _amount);
         s_claimed[_account] = true;
         i_bagleToken.safeTransfer(_account, _amount);
+    }
+    // checks if the signature is valid
+    function _isValidSignature(address account, bytes32 digest, uint8 v, bytes32 r, bytes32 s) public pure returns (bool) {
+        // using Openzeppelin ecrecover
+        (address actualSigner,  , ) = ECDSA.tryRecover(digest, v, r, s);
+        return actualSigner == account;
+    }
+    // get the digest
+    function getMessage(address account, uint256 amount) public view returns (bytes32) {
+        return _hashTypedDataV4(keccak256(abi.encode(MESSAGE_TYPEHASH, AirdropClaim({account: account, amount: amount}))));
     }
 
     function getMerkleRoot() public view returns (bytes32) {
--- test/MerkleAirdrop.t.sol
+++ test/MerkleAirdrop.t.sol
@@ -4,34 +4,50 @@ pragma solidity ^0.8.20;
 import {Test, console} from "forge-std/src/Test.sol";
 import {MerkleAirdrop} from "../src/MerkleAirdrop.sol";
 import {BagleToken} from "../src/BagleToken.sol";
+import {DeployMerkleAirdrop} from "../script/DeployMerkleAirdrop.s.sol";
+import {ZkSyncChainChecker} from "lib/foundry-devops/src/ZkSyncChainChecker.sol"; // checks if contract is deployed on zksync since contracts historically could not be deployed with scripts
 
-contract MerkleAirdropTest is Test {
+
+contract MerkleAirdropTest is ZkSyncChainChecker,   Test {
     // Deploy contract and set as environment variable
     MerkleAirdrop public airdrop;
     BagleToken public token;
     address public user;
     uint256 public userPrivKey;
+    address public gasPayer;
     uint256 public AMOUNT_TO_CLAIM = 25 ether;
     uint256 public AMOUNT_TO_MINT = AMOUNT_TO_CLAIM *4;
     bytes32 public proofOne = 0x0fd7c981d39bece61f7499702bf59b3114a90e66b51ba2c53abdf7b62986c00a;
     bytes32 public proofTwo = 0xe5ebd1e1b5a5478a944ecab36a9a954ac3b6b8216875f6524caa7a1d87096576;
     bytes32[] public PROOF = [proofOne, proofTwo];
     
+    
     bytes32 public merkleRoot = 0xaa5d581231e596618465a56aa0f5870ba6e20785fe436d5bfb82b08662ccc7c4;
 
     function setUp() public {
+        if (isZkSyncChain()) {
+            DeployMerkleAirdrop deployer = new DeployMerkleAirdrop();
+            (airdrop, token) = deployer.deployMerkleAirdrop();
+        } else {
          token = new BagleToken();
          airdrop = new MerkleAirdrop(address(token), merkleRoot); 
          token.mint(token.owner(), AMOUNT_TO_MINT);
          token.transfer(address(airdrop), AMOUNT_TO_MINT);
-         (  user,   userPrivKey) = makeAddrAndKey("user");    
-  
+        }
+        (  user,   userPrivKey) = makeAddrAndKey("user");  
+        gasPayer = makeAddr("gasPayer");
 
     }
     function testUserCanClaim() public { 
         uint256 startingBalance = token.balanceOf(user);
-        vm.prank(user);
-        airdrop.claim(user, AMOUNT_TO_CLAIM, PROOF);
+        bytes32 digest = airdrop.getMessage(user, AMOUNT_TO_CLAIM); // from the MerkleAirdrop contract
+        vm.startPrank(user); // prank the user to sign a message 
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(userPrivKey, digest); // takes private key and returns v, r, s
+        vm.stopPrank();
+
+        vm.startPrank(gasPayer); // now the gaspayer is claiming on behalf of the user
+        airdrop.claim(user, AMOUNT_TO_CLAIM, PROOF, v, r, s);
+        vm.stopPrank();
         uint256 endingBalance = token.balanceOf(user);
         assertEq(endingBalance - startingBalance, AMOUNT_TO_CLAIM);
     }
